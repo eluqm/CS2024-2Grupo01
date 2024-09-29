de2f1907972335d48252ace63db5abb23ec992a6
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-09-28 19:44:21 -0500

    Modularizando los fragments por tipo de usuario.

---
+++ MentoriApp/app/src/main/java/edu/cram/mentoriapp/Common/LoginFragment.kt
@@ -0,0 +1,12 @@
+package edu.cram.mentoriapp.Common
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import edu.cram.mentoriapp.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

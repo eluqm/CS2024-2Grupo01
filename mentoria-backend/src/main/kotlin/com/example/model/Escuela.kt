fbdcb0ca371b2393f8fd92737cc564a36824ada0
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-10-17 01:06:51 -0500

    update backend

--- mentoria-backend/src/main/kotlin/com/example/model/Escuela.kt
+++ mentoria-backend/src/main/kotlin/com/example/model/Escuela.kt
@@ -1,6 +1,5 @@
 package com.example.model
 
 data class Escuela(
-    val escuelaId: Int,
     val nombre: String
 )
--- mentoria-backend/src/main/kotlin/com/example/plugins/EscuelaSchema.kt
+++ mentoria-backend/src/main/kotlin/com/example/plugins/EscuelaSchema.kt
@@ -1,19 +1,30 @@
-package com.example.DAO
+package com.example.plugins
 
-import com.example.model.Escuela
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlinx.serialization.Serializable
 import java.sql.Connection
 import java.sql.Statement
 
-class EscuelasDAO(private val connection: Connection) {
+@Serializable
+data class Escuela(val nombre: String)
+
+class EscuelasService(private val connection: Connection) {
     companion object {
-        private const val INSERT_ESCUELA = "INSERT INTO escuelas (nombre) VALUES (?)"
+        private const val CREATE_TABLE_ESCUELAS =
+            "CREATE TABLE ESCUELAS (ESCUELA_ID SERIAL PRIMARY KEY, NOMBRE VARCHAR(255));"
         private const val SELECT_ESCUELA_BY_ID = "SELECT nombre FROM escuelas WHERE escuela_id = ?"
+        private const val INSERT_ESCUELA = "INSERT INTO escuelas (nombre) VALUES (?)"
         private const val UPDATE_ESCUELA = "UPDATE escuelas SET nombre = ? WHERE escuela_id = ?"
         private const val DELETE_ESCUELA = "DELETE FROM escuelas WHERE escuela_id = ?"
     }
 
+    init {
+        val statement = connection.createStatement()
+        // Descomentar la siguiente línea para crear la tabla si es necesario
+        // statement.executeUpdate(CREATE_TABLE_ESCUELAS)
+    }
+
+    // Crear nueva escuela
     suspend fun create(escuela: Escuela): Int = withContext(Dispatchers.IO) {
         val statement = connection.prepareStatement(INSERT_ESCUELA, Statement.RETURN_GENERATED_KEYS)
         statement.setString(1, escuela.nombre)
@@ -27,30 +38,32 @@ class EscuelasDAO(private val connection: Connection) {
         }
     }
 
-    suspend fun read(escuelaId: Int): Escuela = withContext(Dispatchers.IO) {
+    // Leer una escuela
+    suspend fun read(id: Int): Escuela = withContext(Dispatchers.IO) {
         val statement = connection.prepareStatement(SELECT_ESCUELA_BY_ID)
-        statement.setInt(1, escuelaId)
+        statement.setInt(1, id)
         val resultSet = statement.executeQuery()
 
         if (resultSet.next()) {
-            return@withContext Escuela(
-                nombre = resultSet.getString("nombre")
-            )
+            val nombre = resultSet.getString("nombre")
+            return@withContext Escuela(nombre)
         } else {
-            throw Exception("School not found")
+            throw Exception("Record not found")
         }
     }
 
-    suspend fun update(escuelaId: Int, escuela: Escuela) = withContext(Dispatchers.IO) {
+    // Actualizar una escuela
+    suspend fun update(id: Int, escuela: Escuela) = withContext(Dispatchers.IO) {
         val statement = connection.prepareStatement(UPDATE_ESCUELA)
         statement.setString(1, escuela.nombre)
-        statement.setInt(2, escuelaId)
+        statement.setInt(2, id)
         statement.executeUpdate()
     }
 
-    suspend fun delete(escuelaId: Int) = withContext(Dispatchers.IO) {
+    // Eliminar una escuela
+    suspend fun delete(id: Int) = withContext(Dispatchers.IO) {
         val statement = connection.prepareStatement(DELETE_ESCUELA)
-        statement.setInt(1, escuelaId)
+        statement.setInt(1, id)
         statement.executeUpdate()
     }
 }

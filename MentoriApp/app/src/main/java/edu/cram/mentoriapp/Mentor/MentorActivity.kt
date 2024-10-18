c077c5ac8d7c08c9ab99f0d149b69c6350fdcb31
abeluciano <aaragona@ulasalle.edu.pe>
2024-10-18 10:01:13 -0500

    navigation y bottom navigation de mentor y mentoriado

--- MentoriApp/app/src/main/java/edu/cram/mentoriapp/Mentor/MentorActivity.kt
+++ MentoriApp/app/src/main/java/edu/cram/mentoriapp/Mentor/MentorActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.cram.mentoriapp.R
 
 class MentorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mentor)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationViewMentor)
+
+        val navFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_mentor) as NavHostFragment
+
+        NavigationUI.setupWithNavController(bottomNav, navFragment.navController)
 
     }
 }
--- MentoriApp/app/src/main/java/edu/cram/mentoriapp/Mentoriado/MentoriadoActivity.kt
+++ MentoriApp/app/src/main/java/edu/cram/mentoriapp/Mentoriado/MentoriadoActivity.kt
@@ -5,11 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.cram.mentoriapp.R
 
 class MentoriadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mentoriado)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationViewMentoriado)
+
+        val navFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_mentoriado) as NavHostFragment
+
+        NavigationUI.setupWithNavController(bottomNav, navFragment.navController)
     }
 }

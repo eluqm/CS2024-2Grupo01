bdb3551a95bf3ffd18804bf130aa66f7085ea51e
abeluciano <aaragona@ulasalle.edu.pe>
2024-11-07 08:38:51 -0500

    recycle eventos

--- MentoriApp/app/src/main/java/edu/cram/mentoriapp/Adapter/EventosAdapter.kt
+++ MentoriApp/app/src/main/java/edu/cram/mentoriapp/Adapter/EventosAdapter.kt
@@ -1,7 +1,42 @@
 package edu.cram.mentoriapp.Adapter
 
-class EventosAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import edu.cram.mentoriapp.Model.Evento
+import edu.cram.mentoriapp.R
 
+class EventosAdapter(val items: MutableList<Evento>,
+                     val onItemSelected: (Evento) -> Unit
+): RecyclerView.Adapter<EventosViewHolder>() {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventosViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_event_card,parent, false)
+        return EventosViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: EventosViewHolder, position: Int) {
+        val item = items[position]
+        holder.render(item, onItemSelected)
+    }
+
+    fun addUser(item: Evento) {
+        items.add(0, item)
+        notifyItemInserted(0)
+    }
+
+    private fun deleteUser(index: Int) {
+        items.removeAt(index)
+        notifyItemRemoved(index)
+        notifyItemRangeChanged(index, items.size)
+    }
+
+    fun editUser(index: Int, item: Evento) {
+        items.removeAt(index)
+        items[index] = item
+        notifyItemChanged(index)
+    }
 
 }
--- MentoriApp/app/src/main/java/edu/cram/mentoriapp/Adapter/EventosViewHolder.kt
+++ MentoriApp/app/src/main/java/edu/cram/mentoriapp/Adapter/EventosViewHolder.kt
@@ -1,4 +1,25 @@
 package edu.cram.mentoriapp.Adapter
 
-class EventosViewHolder {
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import edu.cram.mentoriapp.Model.Evento
+import edu.cram.mentoriapp.R
+
+class EventosViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val viewNombre = itemView.findViewById<TextView>(R.id.tv_event_card_title)
+    private val viewDescripcion = itemView.findViewById<TextView>(R.id.tv_event_card_description)
+    private val viewFecha = itemView.findViewById<TextView>(R.id.tv_event_card_date)
+    @SuppressLint("SetTextI18n")
+    fun render(item: Evento, onClickListener:(Evento) -> Unit) {
+        viewNombre.text = item.nombre
+        viewDescripcion.text = item.descripcion
+        viewFecha.text = item.fecha_evento
+
+        itemView.setOnClickListener() {
+            onClickListener(item)
+        }
+    }
 }

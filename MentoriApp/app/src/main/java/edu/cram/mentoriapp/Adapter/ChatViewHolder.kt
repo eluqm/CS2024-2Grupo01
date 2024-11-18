99dbd5f93119b94030aea4d37d7a6ac65832ad7c
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-11-17 23:17:56 -0500

    push 3- mensajes 3

---
+++ MentoriApp/app/src/main/java/edu/cram/mentoriapp/Adapter/ChatViewHolder.kt
@@ -0,0 +1,29 @@
+package edu.cram.mentoriapp.Adapter
+
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import edu.cram.mentoriapp.Model.Chat
+import edu.cram.mentoriapp.Model.Evento
+import edu.cram.mentoriapp.R
+
+class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val viewNombre = itemView.findViewById<TextView>(R.id.txt_emisor_chat)
+    private val viewMensaje = itemView.findViewById<TextView>(R.id.txt_mensaje_chat)
+    private val viewFecha = itemView.findViewById<TextView>(R.id.txt_fecha_chat)
+    private val viewHora = itemView.findViewById<TextView>(R.id.txt_hora_chat)
+
+    @SuppressLint("SetTextI18n")
+    fun render(item: Chat, onClickListener:(Chat) -> Unit) {
+        viewNombre.text = item.emisor
+        viewMensaje.text = item.mensaje
+        viewFecha.text = item.fecha
+        viewFecha.text = item.hora
+
+        itemView.setOnClickListener() {
+            onClickListener(item)
+        }
+    }
+}

5df053bee39a405c0b4e5f2040cadac53550faee
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-11-25 01:08:08 -0500

    push horaios item select

--- MentoriApp/app/src/main/java/edu/cram/mentoriapp/Adapter/HorarioAdapter.kt
+++ MentoriApp/app/src/main/java/edu/cram/mentoriapp/Adapter/HorarioAdapter.kt
@@ -3,13 +3,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import edu.cram.mentoriapp.Model.Evento
+import edu.cram.mentoriapp.Model.GrupoMentoria
 import edu.cram.mentoriapp.Model.HorarioCell
 import edu.cram.mentoriapp.R
 
-class HorarioAdapter(private val celdas: List<HorarioCell>) : RecyclerView.Adapter<HorarioAdapter.HorarioViewHolder>() {
+class HorarioAdapter(private val celdas: List<HorarioCell>, val onItemSelected: (HorarioCell) -> Unit) : RecyclerView.Adapter<HorarioAdapter.HorarioViewHolder>() {
 
     class HorarioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textView: TextView = itemView.findViewById(R.id.cellTextView)
+
+        fun render(item: HorarioCell, onClickListener:(HorarioCell) -> Unit) {
+            itemView.setOnClickListener() {
+                onClickListener(item)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HorarioViewHolder {
@@ -20,6 +28,7 @@ class HorarioAdapter(private val celdas: List<HorarioCell>) : RecyclerView.Adapt
     override fun onBindViewHolder(holder: HorarioViewHolder, position: Int) {
         val cell = celdas[position]
         holder.textView.text = cell.lugar ?: "" // Mostrar el lugar o vacío si no hay evento
+        holder.render(cell, onItemSelected)
     }
 
     override fun getItemCount(): Int = celdas.size

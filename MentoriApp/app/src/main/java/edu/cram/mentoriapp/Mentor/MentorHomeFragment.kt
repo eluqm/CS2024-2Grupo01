1afc5705a159e8317d6c054a57437f3cb5a08a86
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-12-06 12:21:22 -0500

    tablon y mensajeria actualizada

--- MentoriApp/app/src/main/java/edu/cram/mentoriapp/Mentor/MentorHomeFragment.kt
+++ MentoriApp/app/src/main/java/edu/cram/mentoriapp/Mentor/MentorHomeFragment.kt
@@ -2,6 +2,7 @@ package edu.cram.mentoriapp.Mentor
 
 import android.app.AlertDialog
 import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -131,6 +132,33 @@ class MentorHomeFragment : Fragment(R.layout.fragment_mentor_home) {
         val btnCerrarsesion = view.findViewById<ImageButton>(R.id.cerrar_sesion)
 
         val btnMostrarEventos = view.findViewById<ImageButton>(R.id.btn_notification)
+
+        val actividad = requireActivity() as MentorActivity
+        // Funciones que se llaman en cada caso
+        fun onKeyboardShown() {
+            actividad.bottomNav.visibility = View.GONE
+        }
+
+        fun onKeyboardHidden() {
+            actividad.bottomNav.visibility = View.VISIBLE
+            // Acción cuando el teclado se oculta
+        }
+
+        txtMensaje.viewTreeObserver.addOnGlobalLayoutListener {
+            val rect = Rect()
+            txtMensaje.rootView.getWindowVisibleDisplayFrame(rect)
+
+            val screenHeight = txtMensaje.rootView.height
+            val keypadHeight = screenHeight - rect.bottom
+
+            if (keypadHeight > screenHeight * 0.15) {
+                // Teclado visible
+                onKeyboardShown()
+            } else {
+                // Teclado oculto
+                onKeyboardHidden()
+            }
+        }
 
         btnMostrarEventos.setOnClickListener {
             view.findNavController().navigate(R.id.action_mentorHomeFragment_to_mostrarEventosFragment3)

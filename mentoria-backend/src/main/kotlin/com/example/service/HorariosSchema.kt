4a793af14701ce283ada275abab62239a80ac5f3
abeluciano <aaragona@ulasalle.edu.pe>
2024-12-05 21:33:10 -0500

    modificacion del back en horarios

--- mentoria-backend/src/main/kotlin/com/example/service/HorariosSchema.kt
+++ mentoria-backend/src/main/kotlin/com/example/service/HorariosSchema.kt
@@ -18,12 +18,12 @@ data class Horario(
 
 @Serializable
 data class HorarioDetalles(
-    val horarioId: Int,
-    val lugar: String,
-    val dia: String,
-    val horaInicio: String,
-    val horaFin: String,
-    val estado: Boolean,
+    val horarioId: Int? = null,
+    val lugar: String?,
+    val dia: String?,
+    val horaInicio: String?,
+    val horaFin: String?,
+    val estado: Boolean?,
     val nombreGrupo: String?,
     val nombreCompletoJefe: String?,
     val nombreEscuela: String?
@@ -61,20 +61,20 @@ class HorariosService(private val connection: Connection) {
 
 
         private const val SELECT_HORARIOS_DETALLES = """
-SELECT 
-    h.horario_id,
-    h.lugar,
-    h.dia,
-    h.hora_inicio,
-    h.hora_fin,
-    h.estado,
-    COALESCE(g.nombre, '') AS nombre_grupo,
-    COALESCE(u.nombre_usuario || ' ' || u.apellido_usuario, '') AS nombre_completo_jefe,
-    COALESCE(e.nombre, '') AS nombre_escuela
-FROM horarios h
-LEFT JOIN grupos g ON h.horario_id = g.horario_id
-LEFT JOIN usuarios u ON g.jefe_id = u.user_id
-LEFT JOIN escuelas e ON u.escuela_id = e.escuela_id
+    SELECT 
+        h.horario_id,
+        h.lugar,
+        h.dia,
+        h.hora_inicio,
+        h.hora_fin,
+        h.estado,
+        g.nombre AS nombre_grupo,
+        u.nombre_usuario || ' ' || u.apellido_usuario AS nombre_completo_jefe,
+        e.nombre AS nombre_escuela
+    FROM horarios h
+    LEFT JOIN grupos g ON h.horario_id = g.horario_id
+    LEFT JOIN usuarios u ON g.jefe_id = u.user_id
+    LEFT JOIN escuelas e ON u.escuela_id = e.escuela_id
 """
 
 
@@ -188,14 +188,14 @@ LEFT JOIN escuelas e ON u.escuela_id = e.escuela_id
             horarios.add(
                 HorarioDetalles(
                     horarioId = resultSet.getInt("horario_id"),
-                    lugar = resultSet.getString("lugar") ?: "Sin lugar",
-                    dia = resultSet.getString("dia") ?: "Sin día",
-                    horaInicio = resultSet.getString("hora_inicio") ?: "Sin horaI",
-                    horaFin = resultSet.getString("hora_fin") ?: "Sin horaF",
+                    lugar = resultSet.getString("lugar"),
+                    dia = resultSet.getString("dia") ,
+                    horaInicio = resultSet.getString("hora_inicio") ,
+                    horaFin = resultSet.getString("hora_fin") ,
                     estado = resultSet.getBoolean("estado"),
-                    nombreGrupo = resultSet.getString("nombre_grupo") ?: "Sin grupo",
-                    nombreCompletoJefe = resultSet.getString("nombre_completo_jefe") ?: "Sin jefe",
-                    nombreEscuela = resultSet.getString("nombre_escuela") ?: "Sin escuela"
+                    nombreGrupo = resultSet.getString("nombre_grupo"),
+                    nombreCompletoJefe = resultSet.getString("nombre_completo_jefe"),
+                    nombreEscuela = resultSet.getString("nombre_escuela")
                 )
             )
         }
